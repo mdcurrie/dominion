9692e5cf60caafda85a9bc4d1b346cdd1c7ccb47
mdcurrie <mcurrie1@umd.edu>
2020-02-27 00:04:29 -0800

    Add CSS for logging component

--- src/client/Game/index.js
+++ src/client/Game/index.js
@@ -4,19 +4,28 @@ import styles from "./styles.css";
 const Game = ({ connections, status, game, ws, playerId }) => {
   const playerIndex = game.players.findIndex(player => player.id === playerId);
   return (
-    <div>
-      <div className="gameSupply">
-        {game.supply.map(c => (
-          <div
-            className="gameSupplyCard"
-            onClick={() =>
-              ws.send(JSON.stringify({ type: "ASYNC_BUY_CARD", name: c.name }))
-            }
-          >
-            <img className="gameSupplyCardImg" src={`./${c.name}.jpg`} />
-            <div className="gameSupplyCardCount">{c.count}</div>
-          </div>
-        ))}
+    <div className="game">
+      <div className="topRow">
+        <div className="gameSupply">
+          {game.supply.map(c => (
+            <div
+              className="gameSupplyCard"
+              onClick={() =>
+                ws.send(
+                  JSON.stringify({ type: "ASYNC_BUY_CARD", name: c.name })
+                )
+              }
+            >
+              <img className="gameSupplyCardImg" src={`./${c.name}.jpg`} />
+              <div className="gameSupplyCardCount">{c.count}</div>
+            </div>
+          ))}
+        </div>
+        <div className="gameLog">
+          {game.log.map(l => (
+            <div>{l}</div>
+          ))}
+        </div>
       </div>
       <div>
         <div>Hand</div>
@@ -35,12 +44,6 @@ const Game = ({ connections, status, game, ws, playerId }) => {
         <div>{`Actions --- ${game.currentPlayer.actions}`}</div>
         <div>{`Gold --- ${game.currentPlayer.gold}`}</div>
         <div>{`Buys --- ${game.currentPlayer.buys}`}</div>
-      </div>
-      <div>
-        <div>Log</div>
-        {game.log.map(l => (
-          <div>{l}</div>
-        ))}
       </div>
       <button
         onClick={() => ws.send(JSON.stringify({ type: "ASYNC_END_TURN" }))}
