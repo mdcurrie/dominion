a423016b7430868e07e776d08ed67ff96928c9f1
mdcurrie <mcurrie1@umd.edu>
2020-03-25 21:27:22 -0700

    fuck it yolo

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,15 @@
 import path from "path";
 import uuid from "uuid";
 import express from "express";
+import webpack from "webpack";
 import expressWs from "express-ws";
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
 import rootReducer from "../reducers";
 import rootSaga from "../sagas";
+import config from "../../webpack.dev.config.js";
 import { asyncAddConnection, asyncRemoveConnection } from "../actions";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -14,7 +18,8 @@ sagaMiddleware.run(rootSaga);
 
 const app = express(),
   DIST_DIR = __dirname,
-  HTML_FILE = path.join(DIST_DIR, "index.html");
+  HTML_FILE = path.join(DIST_DIR, "index.html"),
+  compiler = webpack(config);
 
 expressWs(app);
 app.ws("/dominion", function(ws, req) {
@@ -46,12 +51,23 @@ app.ws("/dominion", function(ws, req) {
   });
 });
 
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  })
+);
 app.use(express.static("./public"));
+app.use(webpackHotMiddleware(compiler));
 
-app.get("/", (req, res) => {
-  res.set("content-type", "text/html");
-  res.send(HTML_FILE);
-  res.end();
+app.get("/", (req, res, next) => {
+  compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.set("content-type", "text/html");
+    res.send(result);
+    res.end();
+  });
 });
 
 const PORT = process.env.PORT || 8080;
